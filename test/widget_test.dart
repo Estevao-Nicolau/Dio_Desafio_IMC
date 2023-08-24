333abe1c55fce12225e60d0f0711d7153ddf3ab8
nicolau <estevao_nicolau@hotmail.com>
2023-08-24 12:54:03 -0300

    feat:test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,41 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:bmi_calculator/page/home_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:bmi_calculator/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Teste de HomePage Widget', (WidgetTester tester) async {
+    // Constrói nosso widget em uma árvore de widgets de teste
+    await tester.pumpWidget(const MaterialApp(
+      home: HomePage(),
+    ));
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Encontra os widgets de TextField
+    final nomeTextField = find.widgetWithText(TextField, 'Nome:');
+    final pesoTextField = find.widgetWithText(TextField, 'Peso (kg)');
+    final alturaTextField = find.widgetWithText(TextField, 'Altura (0.00)');
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Verifica se os widgets de TextField foram construídos
+    expect(nomeTextField, findsOneWidget);
+    expect(pesoTextField, findsOneWidget);
+    expect(alturaTextField, findsOneWidget);
+
+    // Insere texto nos widgets de TextField
+    await tester.enterText(nomeTextField, 'John Doe');
+    await tester.enterText(pesoTextField, '70.0');
+    await tester.enterText(alturaTextField, '1.75');
+
+    // Encontra o botão "Calcular IMC" e o pressiona
+    final calcularButton = find.widgetWithText(ElevatedButton, 'Calcular IMC');
+    expect(calcularButton, findsOneWidget);
+    await tester.tap(calcularButton);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Encontra o widget de texto resultante e verifica o conteúdo
+    final resultText =
+        find.text('John Doe\nIMC é: 22.86\nClassificação: Saudável');
+    expect(resultText, findsOneWidget);
   });
 }
