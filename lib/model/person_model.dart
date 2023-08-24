b111b626ce5999547aa1fac89802bc8f8dafe67c
nicolau <estevao_nicolau@hotmail.com>
2023-08-24 12:52:08 -0300

    feat:person.model

--- lib/model/person_model.dart
+++ lib/model/person_model.dart
@@ -1,10 +1,18 @@
 class Person {
-  String name;
+  String? name;
   double weight;
   double height;
+
   Person({
     required this.name,
     required this.weight,
     required this.height,
   });
+  double calcularIMC() {
+    if (height <= 0) {
+      return 0.0;
+    }
+    double imc = weight / (height * height);
+    return double.parse(imc.toStringAsFixed(2));
+  }
 }
